9f6f23289ee8c34c68e2f07df22ecbe42a7190e9
Gregory Tippett <gntsketches@gmail.com>
2021-08-02 20:23:38 -0700

    render active keys in piano

--- src/components/Piano/Piano.js
+++ src/components/Piano/Piano.js
@@ -13,7 +13,6 @@ class Piano extends React.Component {
     
     AudioModule.playPitch(note);
     this.props.global.sendResponse(note)
-    
   }
   
   render() {
@@ -26,14 +25,20 @@ class Piano extends React.Component {
           <TouchableOpacity
             onPress={()=> this.playPitch('Cs')}
           >
-            <View style={styles.keyAccidental}>
+            <View style={[
+              styles.keyAccidental, 
+              (activeNotes.includes('Cs')) ? styles.activeKey : null
+            ]}>
               <Text>C# Db</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={()=> this.playPitch('Ds')}
           >
-            <View style={styles.keyAccidental}>
+            <View style={[
+              styles.keyAccidental, 
+              (activeNotes.includes('Ds')) ? styles.activeKey : null
+            ]}>
               <Text>D# Eb</Text>
             </View>
           </TouchableOpacity>
@@ -42,21 +47,30 @@ class Piano extends React.Component {
           <TouchableOpacity
             onPress={()=> this.playPitch('Fs')}
           >
-            <View style={styles.keyAccidental}>
+            <View style={[
+              styles.keyAccidental, 
+              (activeNotes.includes('Fs')) ? styles.activeKey : null
+            ]}>
               <Text>F# Gb</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={()=> this.playPitch('Gs')}
           >
-            <View style={styles.keyAccidental}>
+            <View style={[
+              styles.keyAccidental, 
+              (activeNotes.includes('Gs')) ? styles.activeKey : null
+            ]}>
               <Text>G# Ab</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={()=> this.playPitch('As')}
           >
-            <View style={styles.keyAccidental}>
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('As')) ? styles.activeKey : null
+            ]}>
               <Text>A# Bb</Text>
             </View>
           </TouchableOpacity>
@@ -68,56 +82,80 @@ class Piano extends React.Component {
           <TouchableOpacity
             onPress={()=> this.playPitch('C')}
           >
-            <View style={styles.keyNatural}>
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('C')) ? styles.activeKey : null
+            ]}>
               <Text>C</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={()=> this.playPitch('D')}
           >
-            <View style={styles.keyNatural}>
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('D')) ? styles.activeKey : null
+            ]}>
               <Text>D</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={()=> this.playPitch('E')}
           >
-            <View style={styles.keyNatural}>
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('E')) ? styles.activeKey : null
+            ]}>
               <Text>E</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={()=> this.playPitch('F')}
           >
-            <View style={styles.keyNatural}>
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('F')) ? styles.activeKey : null
+            ]}>
               <Text>F</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={()=> this.playPitch('G')}
           >
-            <View style={styles.keyNatural}>
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('G')) ? styles.activeKey : null
+            ]}>
               <Text>G</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={()=> this.playPitch('A')}
           >
-            <View style={styles.keyNatural}>
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('A')) ? styles.activeKey : null
+            ]}>
               <Text>A</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={()=> this.playPitch('B')}
           >
-            <View style={styles.keyNatural}>
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('B')) ? styles.activeKey : null
+            ]}>
               <Text>B</Text>
             </View>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={()=> this.playPitch('C8')}
           >
-            <View style={styles.keyNatural}>
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('C8')) ? styles.activeKey : null
+            ]}>
               <Text>C</Text>
             </View>
           </TouchableOpacity>
