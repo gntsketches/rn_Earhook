fd999d4c977e4cfffa1a27468b4a4cc7b7d72063
Gregory Tippett <gntsketches@gmail.com>
2021-08-03 07:24:07 -0700

    wip: accidental spacing

--- src/components/Piano/Piano.js
+++ src/components/Piano/Piano.js
@@ -21,65 +21,107 @@ class Piano extends React.Component {
     return (
       <View style={styles.pianoMain}>
         <View style={styles.pianoAccidentalsMain}>
-          <View style={styles.keyPadding}></View>
+
           <TouchableOpacity
-            onPress={()=> this.playPitch('Cs')}
-          >
-            <View style={[
-              styles.keyAccidental, 
-              (activeNotes.includes('Cs')) ? styles.activeKey : null
-            ]}>
-              <Text>C# Db</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={()=> this.playPitch('Ds')}
-          >
-            <View style={[
-              styles.keyAccidental, 
-              (activeNotes.includes('Ds')) ? styles.activeKey : null
-            ]}>
-              <Text>D# Eb</Text>
-            </View>
-          </TouchableOpacity>
-          <View style={styles.keyAccidental}>
-          </View>
-          <TouchableOpacity
-            onPress={()=> this.playPitch('Fs')}
-          >
-            <View style={[
-              styles.keyAccidental, 
-              (activeNotes.includes('Fs')) ? styles.activeKey : null
-            ]}>
-              <Text>F# Gb</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={()=> this.playPitch('Gs')}
-          >
-            <View style={[
-              styles.keyAccidental, 
-              (activeNotes.includes('Gs')) ? styles.activeKey : null
-            ]}>
-              <Text>G# Ab</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={()=> this.playPitch('As')}
+            style={styles.keyAccidentalsOpacityPadding}
+            onPress={()=> this.playPitch('C')}
           >
             <View style={[
               styles.keyNatural, 
-              (activeNotes.includes('As')) ? styles.activeKey : null
+              (activeNotes.includes('C')) ? styles.activeKey : null
             ]}>
-              <Text>A# Bb</Text>
+              {/* <Text>C</Text> */}
             </View>
           </TouchableOpacity>
-          <View style={styles.keyPadding}></View>
-          <View style={styles.keyPadding}></View>
+          <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
+            onPress={()=> this.playPitch('D')}
+          >
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('D')) ? styles.activeKey : null
+            ]}>
+              {/* <Text>C#</Text> */}
+              {/* <Text>Db</Text> */}
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
+            onPress={()=> this.playPitch('E')}
+          >
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('E')) ? styles.activeKey : null
+            ]}>
+              {/* <Text>D#</Text> */}
+              {/* <Text>Eb</Text> */}
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
+            onPress={()=> this.playPitch('F')}
+          >
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('F')) ? styles.activeKey : null
+            ]}>
+              {/* <Text>F</Text> */}
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
+            onPress={()=> this.playPitch('G')}
+          >
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('G')) ? styles.activeKey : null
+            ]}>
+              {/* <Text>F#</Text> */}
+              {/* <Text>Gb</Text> */}
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
+            onPress={()=> this.playPitch('A')}
+          >
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('A')) ? styles.activeKey : null
+            ]}>
+              {/* <Text>G#</Text> */}
+              {/* <Text>Ab</Text> */}
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
+            onPress={()=> this.playPitch('B')}
+          >
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('B')) ? styles.activeKey : null
+            ]}>
+              {/* <Text>A#</Text> */}
+              {/* <Text>Bb</Text> */}
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.keyAccidentalsOpacityPadding}
+            onPress={()=> this.playPitch('C8')}
+          >
+            <View style={[
+              styles.keyNatural, 
+              (activeNotes.includes('C8')) ? styles.activeKey : null
+            ]}>
+              {/* <Text>C</Text> */}
+            </View>
+          </TouchableOpacity>
+
         </View>
 
         <View style={styles.pianoNaturalsMain}>
+
           <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
             onPress={()=> this.playPitch('C')}
           >
             <View style={[
@@ -90,6 +132,7 @@ class Piano extends React.Component {
             </View>
           </TouchableOpacity>
           <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
             onPress={()=> this.playPitch('D')}
           >
             <View style={[
@@ -100,6 +143,7 @@ class Piano extends React.Component {
             </View>
           </TouchableOpacity>
           <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
             onPress={()=> this.playPitch('E')}
           >
             <View style={[
@@ -110,6 +154,7 @@ class Piano extends React.Component {
             </View>
           </TouchableOpacity>
           <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
             onPress={()=> this.playPitch('F')}
           >
             <View style={[
@@ -120,6 +165,7 @@ class Piano extends React.Component {
             </View>
           </TouchableOpacity>
           <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
             onPress={()=> this.playPitch('G')}
           >
             <View style={[
@@ -130,6 +176,7 @@ class Piano extends React.Component {
             </View>
           </TouchableOpacity>
           <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
             onPress={()=> this.playPitch('A')}
           >
             <View style={[
@@ -140,6 +187,7 @@ class Piano extends React.Component {
             </View>
           </TouchableOpacity>
           <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
             onPress={()=> this.playPitch('B')}
           >
             <View style={[
@@ -150,6 +198,7 @@ class Piano extends React.Component {
             </View>
           </TouchableOpacity>
           <TouchableOpacity
+            style={styles.keyAccidentalsOpacity}
             onPress={()=> this.playPitch('C8')}
           >
             <View style={[
@@ -159,6 +208,7 @@ class Piano extends React.Component {
               <Text>C</Text>
             </View>
           </TouchableOpacity>
+
         </View>
 
       </View>
@@ -181,22 +231,32 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     marginBottom: 5,
   },
+  keyAccidentalsOpacityPadding: {
+    flexGrow: 1,
+  },
+  keyAccidentalsOpacity: {
+    flexGrow: 1,
+  },
   keyPadding: {
-    // flex: 1,
+    flexGrow: 1,
     backgroundColor: 'blue',
-    padding: 10,
-    width: 40,
+    // padding: 10,
+    // width: 40,
     height: 100,
   },
+  keyAccidentalWrap: {
+    // padding: 10,
+    height: 100,
+    flexGrow: 1,
+  },
   keyAccidental: {
-    // flex: 2,
     backgroundColor: '#000',
     color: '#fff',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 10,
-    width: 40,
+    // padding: 10,
+    // width: 40,
     height: 100,
     opacity: 0.5,
   },
@@ -224,3 +284,84 @@ const styles = StyleSheet.create({
     opacity: 1,
   },
 })
+
+
+
+
+
+          // <TouchableOpacity>
+          //   <View style={styles.keyPadding}></View>
+          // </TouchableOpacity>
+
+          // <TouchableOpacity 
+          //   style={styles.keyAccidentalWrap}
+          //   onPress={()=> this.playPitch('Cs')}
+          // >
+          //   <View style={[
+          //     styles.keyAccidental, 
+          //     (activeNotes.includes('Cs')) ? styles.activeKey : null
+          //   ]}>
+          //     <Text>C#</Text>
+          //     <Text>Db</Text>
+          //   </View>
+          // </TouchableOpacity>
+
+          // <TouchableOpacity 
+          //   style={styles.keyAccidentalWrap}
+          //   onPress={()=> this.playPitch('Ds')}
+          // >
+          //   <View style={[
+          //     styles.keyAccidental, 
+          //     (activeNotes.includes('Ds')) ? styles.activeKey : null
+          //   ]}>
+          //     <Text>D#</Text>
+          //     <Text>Eb</Text>
+          //   </View>
+          // </TouchableOpacity>
+
+          // <TouchableOpacity>
+          //   <View style={styles.keyPadding}></View>
+          // </TouchableOpacity>
+
+          // <TouchableOpacity 
+          //   style={styles.keyAccidentalWrap}
+          //   onPress={()=> this.playPitch('Fs')}
+          // >
+          //   <View style={[
+          //     styles.keyAccidental, 
+          //     (activeNotes.includes('Fs')) ? styles.activeKey : null
+          //   ]}>
+          //     <Text>F#</Text>
+          //     <Text>Gb</Text>
+          //   </View>
+          // </TouchableOpacity>
+
+          // <TouchableOpacity 
+          //   style={styles.keyAccidentalWrap}
+          //   onPress={()=> this.playPitch('Gs')}
+          // >
+          //   <View style={[
+          //     styles.keyAccidental, 
+          //     (activeNotes.includes('Gs')) ? styles.activeKey : null
+          //   ]}>
+          //     <Text>G#</Text>
+          //     <Text>Ab</Text>
+          //   </View>
+          // </TouchableOpacity>
+
+          // <TouchableOpacity 
+          //   style={styles.keyAccidentalWrap}
+          //   onPress={()=> this.playPitch('As')}
+          // >
+          //   <View style={[
+          //     styles.keyAccidental, 
+          //     (activeNotes.includes('As')) ? styles.activeKey : null
+          //   ]}>
+          //     <Text>A#</Text>
+          //     <Text>Bb</Text>
+          //   </View>
+          // </TouchableOpacity>
+
+          // <TouchableOpacity>
+          //   <View style={styles.keyPadding}></View>
+          // </TouchableOpacity>
